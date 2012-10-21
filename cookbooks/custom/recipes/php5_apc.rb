b2340d4d5b45f1d965cac374123a623a83bdcc31
Uyi Ehondor <uehondor@clickinnovation.co.uk>
2012-10-21 23:34:05 +0100

    Changed php5-apc and php5-mysql recipes to use package

--- cookbooks/custom/recipes/php5_apc.rb
+++ cookbooks/custom/recipes/php5_apc.rb
@@ -1,5 +1,4 @@
-php_pear "apc" do
+package "php-apc" do
     action :install
     package_name "php-apc"
-    directives(:shm_size => 128, :enable_cli => 1)
 end
--- cookbooks/custom/recipes/php5_mysql.rb
+++ cookbooks/custom/recipes/php5_mysql.rb
@@ -1,4 +1,4 @@
-php_pear "mysql" do
+package "php5-mysql" do
     action :install
     package_name "php5-mysql"
 end
